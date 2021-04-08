ca6b137a41117e6bfd280caa121dcbcf5e4b7e2b
ossamaaa <osskhsh@gmail.com>
2021-04-08 21:26:38 +0300

    add css

--- js/app.js
+++ js/app.js
@@ -1,140 +1,140 @@
 
-'use strict';
+// 'use strict';
 
-let score = 0;
-let Name
-function qus1(){
-Name = prompt('what\'s your name?');
-alert('Hi mr/s ' + Name);
-}
-qus1();
-// // let userName = confirm('Are you sure do you want to enter?');
-// // console.log(userName)
-function qus2(){
-let hunger = prompt('Do You think i\'m hangry ?', 'yes/no ');
+// let score = 0;
+// let Name
+// function qus1(){
+// Name = prompt('what\'s your name?');
+// alert('Hi mr/s ' + Name);
+// }
+// qus1();
+// // // let userName = confirm('Are you sure do you want to enter?');
+// // // console.log(userName)
+// function qus2(){
+// let hunger = prompt('Do You think i\'m hangry ?', 'yes/no ');
 
-console.log(hunger)
-hunger = hunger.toLowerCase();
-console.log(hunger)
+// console.log(hunger)
+// hunger = hunger.toLowerCase();
+// console.log(hunger)
 
-if (hunger === 'yes' || hunger === 'y') {
-    alert('You\'r right ' + Name);
-    score++;
-} else if (hunger === 'no' || hunger === 'n') {
-    alert(' no, i\'m always hungry lol ')
-    console.log(hunger);
+// if (hunger === 'yes' || hunger === 'y') {
+//     alert('You\'r right ' + Name);
+//     score++;
+// } else if (hunger === 'no' || hunger === 'n') {
+//     alert(' no, i\'m always hungry lol ')
+//     console.log(hunger);
 
-} else {
+// } else {
 
-    alert('please select again');
-}
-}
-qus2();
-function qus3(){
-let choiceing = confirm('do you think i like sea food?')
+//     alert('please select again');
+// }
+// }
+// qus2();
+// function qus3(){
+// let choiceing = confirm('do you think i like sea food?')
 
-if (choiceing) {
-    alert('yeah i\'m ')
-    score++;
+// if (choiceing) {
+//     alert('yeah i\'m ')
+//     score++;
 
-} else {
-    alert('wrong ,  actually i\'m!')
-}
-}
-qus3();
-function qus4(){
-let junkFood = confirm('Do think I like Junk food?')
-// console.log(junkFood);
-if (junkFood === true) {
-    alert('Right hahah ')
-    score++;
+// } else {
+//     alert('wrong ,  actually i\'m!')
+// }
+// }
+// qus3();
+// function qus4(){
+// let junkFood = confirm('Do think I like Junk food?')
+// // console.log(junkFood);
+// if (junkFood === true) {
+//     alert('Right hahah ')
+//     score++;
 
-} else {
-    alert('no, i do !')
- }
-}
-qus4();
-let gussing=''
-function qus5(){
-for (let i = 1; i < 5; i++) {
+// } else {
+//     alert('no, i do !')
+//  }
+// }
+// qus4();
+// let gussing=''
+// function qus5(){
+// for (let i = 1; i < 5; i++) {
 
-    gussing = prompt('guss my age between 20/25?');
-    gussing=parseInt(gussing)
-    if (gussing == 23) {
+//     gussing = prompt('guss my age between 20/25?');
+//     gussing=parseInt(gussing)
+//     if (gussing == 23) {
 
-        alert('right');
-        score++;
-        break;
-
-    }
-    else if (gussing > 23) {
-
-        alert('too high');
-    }
-
-
-    else if (gussing < 23) {
-        alert('too low');
-    }
-    else {
-        alert('welcome')
-    }
-}
-}
-qus5();
-if(gussing!==23){
-    alert('my age 23!')
-}
-
-
-
-
-
-
-// for (let i = 1; i < 7; i++) {
-
-//     let boysdates = ['1997 ', '1998 ', '1996 ', '1995 '];
-
-//     let choice = prompt('So what\'s my birth? ' + boysdates);
-
-//     if (choice === '1997') {
-//         alert('Right!');
+//         alert('right');
 //         score++;
-
 //         break;
 
-//     } else if (choice)
-//         alert('not correct')
+//     }
+//     else if (gussing > 23) {
+
+//         alert('too high');
+//     }
+
+
+//     else if (gussing < 23) {
+//         alert('too low');
+//     }
+//     else {
+//         alert('welcome')
+//     }
+// }
+// }
+// qus5();
+// if(gussing!==23){
+//     alert('my age 23!')
+// }
+
+
+
+
+
+
+// // for (let i = 1; i < 7; i++) {
+
+// //     let boysdates = ['1997 ', '1998 ', '1996 ', '1995 '];
+
+// //     let choice = prompt('So what\'s my birth? ' + boysdates);
+
+// //     if (choice === '1997') {
+// //         alert('Right!');
+// //         score++;
+
+// //         break;
+
+// //     } else if (choice)
+// //         alert('not correct')
+
+// // }
+// // alert('correct anwser is 1997')
+
+
+// function qus6(){
+// let myFavfood = ['shawarma ', 'mansaf', 'fahita', 'mushroom','checkin','grill','saj'];
+// let favFoood = null;
+
+//  for (let j=0 ;j<6 ;j++){
+
+//  favFoood = prompt('what\'s my favorite food?');
+// favFoood=favFoood.toLowerCase();
+
+// for(let i = 0 ; i < myFavfood.length ; i++ ){
+// if(favFoood === myFavfood[i]){
+
+//     alert('correct !');
+//     j= 6
+//     break;
+// }
 
 // }
-// alert('correct anwser is 1997')
+
+// }
+// }
+//  qus6();
+// let welcoming = confirm('Welcome to page owner ' + Name)
+// console.log(welcoming)
 
 
-function qus6(){
-let myFavfood = ['shawarma ', 'mansaf', 'fahita', 'mushroom','checkin','grill','saj'];
-let favFoood = null;
-
- for (let j=0 ;j<6 ;j++){
-
- favFoood = prompt('what\'s my favorite food?');
-favFoood=favFoood.toLowerCase();
-
-for(let i = 0 ; i < myFavfood.length ; i++ ){
-if(favFoood === myFavfood[i]){
-
-    alert('correct !');
-    j= 6
-    break;
-}
-
-}
-
-}
-}
- qus6();
-let welcoming = confirm('Welcome to page owner ' + Name)
-console.log(welcoming)
-
-
-alert('your scores is :' + score);
+// alert('your scores is :' + score);
 
